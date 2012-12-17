9eedb9b21dd7b38fcbe05c649a4cb0e00f047008
minzhi zhao <minzhi@syncplicity.com>
2012-12-17 15:57:08 -0800

    master modifies class1

--- Class1.cs
+++ Class1.cs
@@ -5,10 +5,10 @@ public class Class1
 	public Class1()
 	{
         boolean t = true;
-        public void testing(){
+        public void testing(int add){
         if(t == true)
         {
-            console.write("Hellow World");
+				console.write("Hellow World; Modified by master");
         }
         else{
             console.write("The is ended");
